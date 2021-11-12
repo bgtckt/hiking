b79be5752c65299aa4395d0b245634f64479c35d
Алексей Анпилов <bgtckt@gmail.com>
2021-11-12 11:23:03 +0300

    Удалил папку dist из gitignore

---
+++ dist/js/levels_slider.js
@@ -0,0 +1,21 @@
+//Реализация слайдера для переключения между уровнями навыков секции levels
+
+const levelsSlider = document.querySelector('#levels-slider');
+const sliderStartValue = Number(levelsSlider.value);
+const sliderStep = Number(levelsSlider.step);
+const levelsInfoBlocks = document.querySelectorAll('.levels__info');
+
+//Настройка первоначальной отрисовки секции
+
+for (let i = 0; i < levelsInfoBlocks.length; i++) {
+  i !== 0 ? levelsInfoBlocks[i].style.display = 'none' : '';
+}
+
+levelsSlider.addEventListener('input', (evt) => {
+  levelsInfoBlocks.forEach(elem => elem.style.display = 'none');
+  for (i = 0; i < levelsInfoBlocks.length; i++) {
+    if (Number(evt.target.value) === sliderStartValue + i * sliderStep) {
+      levelsInfoBlocks[i].style.display = 'block';
+    }
+  }
+});
---
+++ dist/js/scroll_page.js
@@ -0,0 +1,20 @@
+const resourcesLink = document.querySelector('#resources-link');
+const resourcesSection = document.querySelector('.resources');
+const toursLink = document.querySelector('#tours-link');
+const toursSection = document.querySelector('.hiking-tour');
+const classesLink = document.querySelector('#classes-link');
+const classesSection = document.querySelector('.basic-classes');
+const contactsLink = document.querySelector('#contacts-link');
+const contactsSection = document.querySelector('.page-footer');
+
+function scrollPage(link, section) {
+  link.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    section.scrollIntoView({behavior: "smooth"});
+  });
+};
+
+scrollPage(resourcesLink, resourcesSection);
+scrollPage(toursLink, toursSection);
+scrollPage(classesLink, classesSection);
+scrollPage(contactsLink, contactsSection);
